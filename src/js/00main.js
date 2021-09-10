40bc314582209f3a41e257e3f80da9c0d6d2bd4b
Sararid <sarawen88@gmail.com>
2021-09-10 14:27:58 +0200

    add first draft for creating function fetch + renderlist

--- src/js/00main.js
+++ src/js/00main.js
@@ -9,28 +9,33 @@ const searchBtn = document.querySelector('.js_btn');
 
 const seriesListHtml = document.querySelector('.js_movielist');
 
+const favoriteListHtml = document.querySelector('.js_favoriteList');
+
 const placeholderImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
 let listSeries = [];
 
+let favoriteSeries = [];
 
 
 //CREAR FUNCTIONS FETCH 
 
-function handleSearch() {
+function handleApiFetch(e) {
+  e.preventDefault();
+
   fetch(`https://api.tvmaze.com/search/shows?q=:${userInput.value}`)
     .then(response => response.json())
 
-    .then(data => {
-      //seriesList.innerHTML = "";
-      listSeries = data;
-      console.log(data);
-      renderList();
-
+    .then(dataShows => {
+      listSeries = dataShows.map(data => {
+        return { id: data.show.id, name: data.show.name, image: data.show.image };
+      });
+      renderListShow();
+      //.catch(error => console.log(`Ha sucedido un error: ${error}`));
     });
-  //  .catch(error => console.log(`Ha sucedido un error: ${error}`));
+
 }
 
 
-searchBtn.addEventListener('click', handleSearch);
+searchBtn.addEventListener('click', handleApiFetch);
 
--- src/js/01renderListShow.js
+++ src/js/01renderListShow.js
@@ -1,20 +1,24 @@
 'use strict';
 //funcion para pintar las series en el html 
-function renderList() {
+function renderListShow() {
   seriesListHtml.innerHTML = "";
-  for (let i in listSeries) {
 
-    const serieName = listSeries[i].show.name;
-    const serieId = listSeries[i].show.id;
+  for (let list of listSeries) {
+
+    const movieName = list.name;
+    const movieId = list.id;
+
     let newItemList = document.createElement('li');
     let img = document.createElement('img');
     let textName = document.createElement('p');
-    let textField = document.createTextNode(serieName);
-    newItemList.id = serieId; //añado el id de la serie al li
-    newItemList.className = 'styleList js_li'; //añado el estilo 
 
-    if (listSeries[i].show.image !== null) {
-      let getImage = listSeries[i].show.image.medium;
+    let textField = document.createTextNode(movieName);
+
+    newItemList.id = movieId;
+    newItemList.className = 'styleList js_li';
+
+    if (list.image !== null) {
+      let getImage = list.image.medium;
       img.src = getImage;
     } else {
       img.src = placeholderImage;
@@ -27,4 +31,5 @@ function renderList() {
     seriesListHtml.appendChild(newItemList);
 
   }
+
 }
